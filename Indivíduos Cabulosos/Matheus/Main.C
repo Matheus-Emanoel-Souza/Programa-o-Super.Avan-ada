35221229b297194a9f9af32512a3b80ed7f81af8
matheus <matheus.souza@ogmoes.com.br>
2024-05-03 13:58:19 -0300

    #Atualização

--- Indivíduos Cabulosos/Matheus/Main.C
+++ Indivíduos Cabulosos/Matheus/Main.C
@@ -10,12 +10,25 @@ typedef struct tela{
     char titulo[40];
     char info[45];
     int opcao;
+    TIPOTELA tipo;
 }TELA;
 //Aqui estou tentando copiar a lógica de "Classe" que se utiliza em C#. A classe apresentará essas seguintes informações -> um titulo, uma informação e o numero da opção.
 
 TELA tela_1[3]=
 {
-{"--Menu","( )PC e ( )ARDUINO",0,MULT,},
-{"ESCREVA SUA MENSANGE",0},
+{"--Menu\n","( )PC e ( )ARDUINO",0,MULT},
+{"ESCREVA SUA MENSANGE",0,TEXT},
 {"TELA 3", "1 p/ menu, 2 p/ VOLTAR, 3 Sair",0,MULT},
 };
+
+TELA tela_2[3]=
+{
+
+};
+
+TELA tela_3[3]=
+{
+
+};
+
+//aperta o otão 1 vez para selecionar e 2 para confirmar a opção!!!
--- Indivíduos Cabulosos/Matheus/Projeto-Arduino.C
+++ Indivíduos Cabulosos/Matheus/Projeto-Arduino.C
@@ -1,50 +1,41 @@
-# include<LiquidCrystal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
 
-#define BOTAO_1 2
-#define BOTAO_2 3
-//AQUI EU DEFINO QUAIS PINOS CADA BOTÃO ESTÁ LOCALIZADO.
+typedef enum tipo{
+    MULT,TEXT
+}TIPOTELA;
 
-int contador_botao_1 = 0;
-int contador_botao_2 = 0;
-//AQUI EU COLOCO UM CONTADOR PARA CADA BOTÃO, A IDEIA É UTILIZAR A MATEMATICA DE LAMPADA.
+typedef struct tela{
+    char titulo[40];
+    char info[45];
+    int opcao;
+    TIPOTELA tipo;
+}TELA;
+//Aqui estou tentando copiar a lógica de "Classe" que se utiliza em C#. A classe apresentará essas seguintes informações -> um titulo, uma informação e o numero da opção.
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-//DEFINI OS PINOS QUE O LCD ESTÁ LOCALIZADO.
-
-void setup() 
+TELA tela_1[3]=
 {
-    lcd.begin(16,2);//AQUI EU CHAMO A FUNÇÃO DA BIBLIOTECA(CRISTALlIQUID) E PASSO O TAMANHO DA TELA DE LCD 16 POR 2
-    pinMode(BOTAO_1, INPUT);
-    pinMode(BOTAO_2, INPUT);
-    lcd.setCursor(0,0);//TESTAR RETIRAR ESTE ARGUMENTO.
-    lcd.print("Contador 1: 0");
-    lcd.setCursor(0,1);
-    lcd.print("Contador 2: 0");
-}
+{"--Menu\n","( )PC e ( )ARDUINO",0,MULT},
+};
 
-void loop
+TELA tela_2[3]=
 {
-  if (digitalRead(BOTAO_1) == HIGH) 
-  {
-    delay(50);
-    contador_botao_1++;
-    atualizarLCD();
-  }
+{"Escolha uma Opção:\n","( )ENVIAR MENSAGEM  ( )RECEBER MENSAGEM",0,MULT}
+};
 
-  if (digitalRead(BOTAO_2) == HIGH) 
-  {
-    delay(50);
-    contador_botao_2++;
-    atualizarLCD();
-  }
-}
+TELA tela_3[3]=
+{
+{"ENVIAR MENSAGEM:\n:","1- P/ Escrever, 2-P/ Confirmar",0,MULT}
+};
 
-void atualizarLCD() {
-  lcd.clear();
-  // Exibe os contadores atualizados no LCD
-  lcd.print("Contador 1: ");
-  lcd.print(contador_botao_1);
-  lcd.setCursor(0, 1);
-  lcd.print("Contador 2: ");
-  lcd.print(contador_botao_2);
+TELA tela_4[3]=
+{
+{"ler mensagem","Mensangem:",0,TEXT}
+};
+
+void main()
+{
+  
 }
+//aperta o otão 1 vez para selecionar e 2 para confirmar a opção!!!
