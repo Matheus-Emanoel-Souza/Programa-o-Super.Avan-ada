2b8c4fc6b14d89c0fdbaa2723d7e8ec12aa87209
matheus <matheus.souza@ogmoes.com.br>
2024-07-01 09:05:33 -0300

    Remoção de Variáveis Globais. Passagem de Parâmetros. <hSerial agora é um argumento passado para as funções relevantes em vez de ser uma variável global.
    As funções loop, menu, preparar_para_pc e escrever_mensagem agora aceitam hSerial e historico_msg como parâmetros, evitando o uso de variáveis globais.>

--- Projeto-PC.c
+++ Projeto-PC.c
@@ -3,104 +3,76 @@
 #include <string.h>
 #include <windows.h>
 #include <stdbool.h>
-#include <time.h>  
+#include <time.h>
 
 #define BOTAO_NAVE 8
 #define BOTAO_CONF 9
 
-typedef struct enum {
+typedef enum {
     Arduino,
-    pc;
-    }Remetente;
+    PC
+} Remetente;
 
-typedef struct mensage
-{
+typedef struct {
     char msg[36];
-    Remetente Remet;
-    //bool EnviadoPeloArduino;   
-} mensage;
+    Remetente remet;
+} Mensage;
 
-No CriaNoRecebido(mensage msg)
-{
-    No NovoNo;
-    NovoNo.msg = msg.msg;
-    NovoNo.next = NULL;
-    NovoNo.msg.Remet = 0;
-    return NovoNo;
-}
-No CriaNoEnviado(mensage msg)
-{
-    No NovoNo;
-    NovoNo.msg = msg.msg;
-    NovoNo.next = NULL;
-    NovoNo.msg.Remet = 1;
-    return NovoNo;
-}
-typedef struct No 
-{
-    mensage msg;             
-    struct No* next;    
+typedef struct No {
+    Mensage msg;
+    struct No* next;
 } No;
-typedef struct Lista
-{
-    No*primeiro;
-    No*ultimo;
-    No*marcador;
-    int contador;
-}Lista;
 
-Lista cria_lista()
-{
-    Lista nova_lista={NULL, NULL,NULL,0};
+typedef struct {
+    No* primeiro;
+    No* ultimo;
+    int contador;
+} Lista;
+
+Lista cria_lista() {
+    Lista nova_lista = {NULL, NULL, 0};
     return nova_lista;
 }
 
-void add_na_lista(No* no, Lista* lista)
-{
-if (lista->contador == 0)
-{
-    lista->primeiro = no;
-    lista->marcador = no;
-}
-else{
-    lista->marcador->next = no;
-}
-lista->contador++;
-}
-
-
-HANDLE hSerial;
-
-void setup();
-void loop();
-void Menu();
-void prepararParaPC();
-void iniciarSerial();
-void fecharSerial();
-void enviarDados(const char *data);
-void receberDados(char *buffer, int length);
-void delay(unsigned int milliseconds);  // Ajustado para definir o tipo de argumento
-
-int main() 
-{
-    iniciarSerial();
-    setup();
-    while (1) {
-        loop(HistoricoMsg);
-        //Emprimir Lista.
+No* cria_no(Mensage msg, Remetente remet) {
+    No* novo_no = (No*)malloc(sizeof(No));
+    if (novo_no == NULL) {
+        printf("Erro ao alocar memória\n");
+        exit(EXIT_FAILURE);
     }
-
-    fecharSerial();
-    return 0;
+    novo_no->msg = msg;
+    novo_no->msg.remet = remet;
+    novo_no->next = NULL;
+    return novo_no;
 }
 
-void delay(unsigned int milliseconds) {
-    clock_t start_time = clock();
-    while (clock() < start_time + milliseconds * CLOCKS_PER_SEC / 1000);
+void add_na_lista(No* no, Lista* lista) {
+    if (lista->contador == 0) {
+        lista->primeiro = no;
+        lista->ultimo = no;
+    } else {
+        lista->ultimo->next = no;
+        lista->ultimo = no;
+    }
+    lista->contador++;
 }
 
-void iniciarSerial() {
-    hSerial = CreateFile(
+void imprime_msg(Mensage msg) {
+    printf("------------------------\n");
+    printf("Fonte: %s\n", msg.remet == Arduino ? "Arduino" : "PC");
+    printf("Mensagem: %s\n", msg.msg);
+}
+
+void exibir_historico(Lista lista) {
+    No* marcador = lista.primeiro;
+    while (marcador != NULL) {
+        imprime_msg(marcador->msg);
+        marcador = marcador->next;
+    }
+}
+
+HANDLE iniciar_serial() {
+    HANDLE hSerial = CreateFile(
         "COM8",
         GENERIC_READ | GENERIC_WRITE,
         0,
@@ -146,20 +118,23 @@ void iniciarSerial() {
         printf("Erro ao definir tempos limite\n");
         exit(EXIT_FAILURE);
     }
+    
+    return hSerial;
 }
 
-void fecharSerial() {
+void fechar_serial(HANDLE hSerial) {
     CloseHandle(hSerial);
 }
 
-void enviarDados(const char *data) {
+void enviar_dados(HANDLE hSerial, const char *data) {
     DWORD bytes_written;
     if (!WriteFile(hSerial, data, strlen(data), &bytes_written, NULL)) {
         printf("Erro ao gravar na porta serial\n");
+        iniciar_serial();
     }
 }
 
-void receberDados(char *buffer, int length) {
+void receber_dados(HANDLE hSerial, char *buffer, int length) {
     DWORD bytes_read;
     if (!ReadFile(hSerial, buffer, length, &bytes_read, NULL)) {
         printf("Erro ao ler a partir da porta serial\n");
@@ -167,18 +142,21 @@ void receberDados(char *buffer, int length) {
     buffer[bytes_read] = '\0';
 }
 
+void delay_ms(unsigned int milliseconds) {
+    clock_t start_time = clock();
+    while (clock() < start_time + milliseconds * CLOCKS_PER_SEC / 1000);
+}
+
 void setup() {
     printf("Inicialização\n");
-    Menu();
 }
 
-void loop() {
-    Lista HistoricoMsg = cria_lista();
-    delay(1000);
-    Menu(HistoricoMsg);
+void loop(Lista* historico_msg, HANDLE hSerial) {
+    delay_ms(1000);
+    menu(historico_msg, hSerial);
 }
 
-void Menu(Lista HistoricoMsg) {
+void menu(Lista* historico_msg, HANDLE hSerial) {
     printf("-----BEM VINDO-----\n");
     printf("1-ENVIAR 2-RECEBER\n");
 
@@ -186,59 +164,48 @@ void Menu(Lista HistoricoMsg) {
     scanf("%d", &escolha);
 
     if (escolha == 1) {
-       prepararParaPC(HistoricoMsg);
+        preparar_para_pc(historico_msg, hSerial);
     } else if (escolha == 2) {
-        escrevermensagem(HistoricoMsg);
+        escrever_mensagem(historico_msg, hSerial);
     }
 }
 
-void prepararParaPC(Lista HistoricoMsg) {
+void preparar_para_pc(Lista* historico_msg, HANDLE hSerial) {
     char mensagem[32];
-    receberDados(mensagem, 32);
+    receber_dados(hSerial, mensagem, 32);
     printf("Mensagem recebida: %s\n", mensagem);
-    enviarDados(mensagem);
-    No Msg_Enviada = CriaNoRecebido(mensagem);
-    add_na_lista(Msg_Enviada,HistoricoMsg);
-    printf("\n");
+    enviar_dados(hSerial, mensagem);
+    Mensage msg = {.remet = Arduino};
+    strncpy(msg.msg, mensagem, sizeof(msg.msg));
+    No* msg_recebida = cria_no(msg, Arduino);
+    add_na_lista(msg_recebida, historico_msg);
 }
-void escrevermensagem(Lista HistoricoMsg) {
-    bool escolhaConcluida = false;
 
-    while (!escolhaConcluida) {
-        char escolha[16];
+void escrever_mensagem(Lista* historico_msg, HANDLE hSerial) {
+    bool escolha_concluida = false;
+
+    while (!escolha_concluida) {
+        char escolha[36];
         scanf("%s", escolha);
 
-        int vogais_count = 5;
-        if (vogais_count == 5) {
-            printf("FOI ENVIADO\n");
-            printf("MSG: %s\n", escolha);
-            No Msg_Enviada = CriaNoEnviado(escolha);
-            add_na_lista(Msg_Enviada,HistoricoMsg);
-            enviarDados(escolha);
-            escolhaConcluida = true;
-            break;
-        }
+        printf("FOI ENVIADO\n");
+        printf("MSG: %s\n", escolha);
+        Mensage msg = {.remet = PC};
+        strncpy(msg.msg, escolha, sizeof(msg.msg));
+        No* msg_enviada = cria_no(msg, PC);
+        add_na_lista(msg_enviada, historico_msg);
+        enviar_dados(hSerial, escolha);
+        escolha_concluida = true;
     }
 }
-void ImprimeMsg(mensage msg)
-{
-    printf("------------------------\n");
-    if (msg.Remet == 0)
-    {
-        printf("Fonte: Arduino");
+
+int main() {
+    HANDLE hSerial = iniciar_serial();
+    setup();
+    Lista historico_msg = cria_lista();
+    while (1) {
+        loop(&historico_msg, hSerial);
     }
-    else{
-        printf("Fonte: PC");
-    }
-    printf("Mensagem:%s",msg.msg);
-}
-void ExibirHistórico(Lista lista)
-{
-    lista.marcador = lista.primeiro;
-    while (lista.marcador != NULL)
-    {
-        ImprimeMsg(lista.marcador->msg)
-        lista.marcador = lista.marcador->next;
-    }
-    
+    fechar_serial(hSerial);
+    return 0;
 }
